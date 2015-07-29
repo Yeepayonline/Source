174b9a2e5201017870e05de9616bcb5bb214cc79
Yeepayonline <zaodaodeqiutian@126.com>
2015-07-29 11:57:04 +0800

    更新

--- 一键支付/一键支付标准版-手机端商户接入包 V1.08/一键支付标准版-手机端商户接入包 V1.08/InstantPay-paymobile/WEB-INF/classes/com/yeepay/ConfigureEncryptAndDecrypt.java
+++
@@ -1,8 +0,0 @@
-package com.yeepay;
-
-public class ConfigureEncryptAndDecrypt {
-	public static final String CHAR_ENCODING = "UTF-8";
-	public static final String AES_ALGORITHM = "AES/ECB/PKCS5Padding";
-	public static final String RSA_ALGORITHM = "RSA/ECB/PKCS1Padding";
-}
-
